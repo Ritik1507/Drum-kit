2ccb89acc9827fadca5944cf0139e15045300808
ritik padhi <ritikpadhi1507@@example.com>
2023-05-28 15:16:40 +0530

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+var noOfButtons = document.querySelectorAll(".drum").length;
+
+for(var i =0;i<noOfButtons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",
+    function(){
+        alert("clicked");
+    })
+}
